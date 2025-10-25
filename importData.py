f7ca64abe8f96583ff064a450c74a48adda61b02
jwrightd <jameswright112106@gmail.com>
2025-10-25 15:43:31 -0400

    lemma

--- importData.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-file_path = 'WELFake_Dataset.csv'
-df = pd.read_csv(file_path)
-
-
--- lemmatization.py
+++ lemmatization.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from nltk.stem import WordNetLemmatizer
 
 path = "WELFake_Dataset.csv"
 df = pd.read_csv(path)
 print(df.head())
+
+
