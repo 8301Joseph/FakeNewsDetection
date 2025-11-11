153912afeddb8b3ec908a35de6f41b25078dc9f2
Evan Pagtalunan <reese.pagtalunan@duke.edu>
2025-11-10 22:33:53 -0500

    Revert "Merge branch 'main' into reese/doc2vec"
    This reverts commit 36e646d5e8852deb95372edd7071896ae6087746, reversing
    changes made to e98deb9c59435cc4bcc475da8244f3547070a418.

--- sentence2vec.py
+++ sentence2vec.py
@@ -1,16 +1,19 @@
+from sent2vec.vectorizer import Vectorizer
+from scipy import spatial
+import pandas as pd
+
 #https://pypi.org/project/sent2vec/
 
-import re
-import pandas as pd
-from sent2vec.vectorizer import Vectorizer
+def sentence2vec(sentences):
+    vectorizer = Vectorizer()
+    vectorizer.run(sentences)
+    vectors = vectorizer.vectors
+    return vectors
 
-SENT_COL = "Cleaned text"  # your column
-
-def split_into_sentences(text: str):
-    if not isinstance(text, str):
-        text = "" if pd.isna(text) else str(text)
-    parts = re.split(r'(?<=[.!?])\s+', text.strip())
-    return [p.strip() for p in parts if p.strip()]
+def apply_s2v(path):
+    df = pd.read_csv(path)
+    df['Vectorized'] = df['text'].apply(sentence2vec) 
+    df.to_csv("sentence2vec.csv")
 
 path = "WELFake_Dataset.csv"
 sentence2vec(path)
